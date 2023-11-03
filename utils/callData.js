10859ff52b8d3642c1f2ac9007cdea899f339c8d
mckiller1999 <lrhoag123@gmail.com>
2023-11-03 15:28:41 +0700

    update-again-XD

--- utils/callData.js
+++ utils/callData.js
@@ -1,10 +1,12 @@
 const arrList = document.querySelectorAll(".nav-item.nav-link");
 //console.log(arrList);
+//dùng for in để lọc ra các thẻ a có trong div nav-item
 for (let i in arrList) {
+  //console.log(arrList[i]); => ra các thẻ a có trong div nav-item
   arrList[i].onclick = function () {
-    //dùng for of để lọc ra các phần tử trong button
+    //dùng for of để lọc ra các phần tử button có trong arrList[i]
     for (let button of arrList) {
-      //console.log(button); => ra các btn có trong mảng arrList
+      //console.log(button); => ra các btn có trong mảng arrList[i]
       button.classList.remove("active");
     }
     this.classList.add("active");

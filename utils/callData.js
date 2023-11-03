29af74b176f4bc973415bb404a66861037d2a335
mckiller1999 <lrhoag123@gmail.com>
2023-11-03 13:21:23 +0700

    update

--- utils/callData.js
+++ utils/callData.js
@@ -5,10 +5,10 @@ for (let i in arrList) {
   arrList[i].onclick = function () {
     //dùng for of để lọc ra các phần tử trong button
     for (let button of arrList) {
-      //console.log(button); => ra các btn có trong mảng arrA
+      //console.log(button); => ra các btn có trong mảng arrList
       button.classList.remove("active");
     }
-    // arrList.forEach((button) => {});
+
     this.classList.add("active");
 
     createItem(show[i], itemShow[i], res[i]);
